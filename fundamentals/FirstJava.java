80b230150eaabfa6ef9e745f45443dc1e1d1223c
Brett A. Jory <brett.austin.jory@gmail.com>
2019-07-04 17:57:25 -0700

    restarted repository with gitignore file

---
+++ fundamentals/FirstJava.java
@@ -0,0 +1,9 @@
+public class FirstJava
+{
+  public static void main(String[] args)
+  {
+    System.out.println("My name is Brett Jory");
+    System.out.println("I am 26 years old");
+    System.out.println("My hometown is Seattle, WA");
+  }
+}
---
+++ fundamentals/Pythagorean.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+public class Pythagorean
+{
+  public double calculateHypotenuse(int legA, int legB)
+  {
+    return Math.sqrt((legA * legA) + (legB * legB));
+  }
+}
